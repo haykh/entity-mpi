d84c090b8befe5e0b176465fd7dac8495a247fa9
jmahlmann <jens.mahlmann@gmail.com>
2023-01-04 11:40:29 -0500

    Structure of toy setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Kokkos_Core.hpp>
-
 #include <iostream>
-
 #include <adios2.h>
 #if ADIOS2_USE_MPI
 #include <mpi.h>
@@ -10,27 +8,14 @@
 
 auto main(int argc, char* argv[]) -> int {
   
-  int N = 10;
-  double result;
-
   Kokkos::initialize(argc, argv);
   {
 
-  Kokkos::View<double*> y( "y", N );
+  // Initialize mesh
 
-  Kokkos::parallel_for( "y_init", N, KOKKOS_LAMBDA ( int i ) {
-    y( i ) = 1;
-  });
+  // Advance physical time
 
-  Kokkos::parallel_for( "y_add", N, KOKKOS_LAMBDA ( int i ) {
-    y( i ) += 0.1;
-  });
-  
-  Kokkos::parallel_reduce("Loop1", N, KOKKOS_LAMBDA (const int& i, double& lsum ) {
-    lsum += y( i );
-  },result);
-
-  std::cout << result << std::endl;
+  // Output data
 
   }
   Kokkos::finalize();
